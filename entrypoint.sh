5c61779137c4c9d96aab2110ea75d80f69b64ae4
Yi Ou <ouyi@gmx.de>
2016-12-16 13:44:22 +0000

    Pass user name and user home as env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,14 @@
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or fallback
 
-USER_ID=${LOCAL_USER_ID:-9001}
+user_id=${LOCAL_USER_ID:-9001}
+user_name=${LOCAL_USER_NAME:-"user"}
+user_home=${LOCAL_USER_HOME:-"/home/$user_name"}
 
-echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m coder
-export HOME=/home/coder
+echo "user_id: $user_id"
+echo "user_name: $user_name"
+echo "user_home: $user_home"
+useradd --shell /bin/bash -u $user_id -o -c "" -m $user_name
+export HOME="$user_home"
 
-exec /usr/local/bin/gosu coder "$@"
+exec /usr/local/bin/gosu "$user_name" "$@"
